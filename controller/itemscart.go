898b0045c7e38255dd438ab38d579134020ea4eb
AiJook <SutthikanwithB@gmail.com>
2025-03-26 16:51:48 +0700

    jjj

--- controller/itemscart.go
+++ controller/itemscart.go
@@ -12,6 +12,7 @@ var Idb *gorm.DB
 func ItemCartController(router *gin.Engine, db *gorm.DB) {
 	Idb = db
 	router.GET("/itemscart/:id", itemsCart)
+	router.POST("/search", searchItems)
 }
 
 func itemsCart(c *gin.Context) {
@@ -24,4 +25,29 @@ func itemsCart(c *gin.Context) {
 	}
 
 	c.JSON(200, carts)
+}
+
+func searchItems(c *gin.Context) {
+	var input struct {
+		Description string  `json:"description"`
+		MinPrice    float64 `json:"min_price"`
+		MaxPrice    float64 `json:"max_price"`
+	}
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	var items []model.Product
+
+	query := Idb.Model(&items)
+	query = query.Where("description LIKE ? AND price >= ? and price <= ?", "%"+input.Description+"%", input.MinPrice, input.MaxPrice)
+
+	if err := query.Find(&items).Error; err != nil {
+		c.JSON(404, gin.H{"error": "No products found"})
+		return
+	}
+
+	c.JSON(200, items)
 }
